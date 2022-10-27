cdf0aea4a5af4a3242c6fdb7b52eeef2e08b2b2a
LuisFelipeAP <lusfelipea@gmail.com>
2022-10-27 11:36:45 -0300

    navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,38 @@
 import "./navbar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>socialapp</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://thispersondoesnotexist.com/image" alt="avatar" />
+          <span>Lorem Ipsum</span>
+        </div>
+      </div>
+    </div>
   )
 }
